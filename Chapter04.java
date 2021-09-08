1ec698620408ee79d8a1832a939d1e3796cb2f24
siryu-saito <saito.s.ryu@gmail.com>
2021-09-08 21:51:56 +0900

    Java 5章まで

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "大谷翔平";
+		System.out.println(name);
+		
+		name = "継続は力なり";
+		System.out.println(name);
+		
+		final int number = 11;
+		System.out.println(number);
+	}
+}
---
+++ Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123L;
+		double double1 = 1.23d;
+		boolean boolean1 = true;
+
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.1f;
+		Integer integer1 = 123;
+		Character character1 = 'あ';
+
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+
+		int[] int1 = { 10, 20, 30, 40, 50};
+		System.out.println(int1[3]);
+	}
+}
