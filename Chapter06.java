f4d74950c51bcbf3df46e3efe775bd4103faabd7
siryu-saito <saito.s.ryu@gmail.com>
2021-09-09 17:28:03 +0900

    Java 7章

--- Chapter06.java
+++ Chapter06.java
@@ -4,16 +4,16 @@ public class Chapter06 {
 		int val = 2 + 5;
 		System.out.println(val);
 
-		++val;
+		val++;
 		System.out.println(val);
 
 		val *= 5;
 		System.out.println(val);
 
-		boolean bool = val == 50;
+		boolean bool = (val == 50);
 		System.out.println(bool);
 
-		bool = val < 50 && val % 10 == 0;
+		bool = (val < 50) && (val % 10 == 0);
 		System.out.println(bool);
 	}
 }
---
+++ Chapter07.java
@@ -0,0 +1,19 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		String signal = "yellow";
+		switch (signal) {
+			case "red":
+				System.out.println("赤信号です");
+				break;
+			case "yellow":
+				System.out.println("黄信号です");
+				break;
+			case "blue":
+				System.out.println("青信号です");
+				break;
+			default:
+				System.out.println("信号は点灯しておりません");
+		}
+	}
+}
